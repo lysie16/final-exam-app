bb27df24d2053ffb06feea81f86ee82e90ea578d
lysie16 <lysie16@hotmail.ca>
2021-12-15 10:39:05 -0500

    Created Logout component, Sidebar components, and posts component

---
+++ src/components/SidebarRow/index.jsx
@@ -0,0 +1,13 @@
+import './styles.css';
+
+export const SidebarRow = ({ImageLink,title, dropdown, avatar}) => {
+    return (
+        <div className="sidebarRow" >
+             <img src={ImageLink} alt="" className={`sidebarRow__icon ${avatar && "avatar"} ${dropdown && 'vanish'}`} />
+            <div className={`dropdownDiv ${dropdown && "display"}`}>
+                <i className={`dropdown ${dropdown && "display"}`} />
+            </div>
+            <h2 className={`sidebarRow__title`}>{title}</h2>
+        </div>
+    )
+}
